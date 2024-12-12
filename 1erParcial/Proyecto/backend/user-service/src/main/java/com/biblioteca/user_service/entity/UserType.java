b512df396d6e867c074d2a2724cc661c8cb28a6a
JohnyNavarro2 <jhonnavarro000@gmail.com>
2024-12-12 07:55:39 -0500

    .

--- 1erParcial/Proyecto/backend/user-service/src/main/java/com/biblioteca/user_service/entity/UserType.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.user_service.entity;
-
-public enum UserType {
-    ADMIN,
-    STUDENT,
-    TEACHER
-}
--- 1erParcial/Proyecto/database/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Ejecutar el entrypoint original de PostgreSQL
-exec docker-entrypoint.sh "$@"
-
-chmod +x docker-entrypoint.sh
